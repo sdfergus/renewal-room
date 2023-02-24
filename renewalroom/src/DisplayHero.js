8420ea098d3c6d641048033ad18e64047e952221
Shandira <shandira.ferguson@gmail.com>
2023-02-24 15:43:40 -0800

    CTA button altered to pill

--- renewalroom/src/DisplayHero.js
+++ renewalroom/src/DisplayHero.js
@@ -6,7 +6,7 @@ function DisplayHero( props ) {
       <div className='hero-text'>
         <h1>The Renewal Room</h1>
         <p>Indulge in the ultimate relaxation experience today! Browse our range of services and book now to discover the many ways we can help you unwind, rejuvenate, and renew</p>
-        <button type="button" className='btn btn-rounded button-cta' onClick={() => { props.scroll( props.ctaRef ) }}>View Services</button>
+        <button type="button" className='btn rounded-pill button-cta' onClick={() => { props.scroll( props.ctaRef ) }}>View Services</button>
       </div>
     </header>
   );
