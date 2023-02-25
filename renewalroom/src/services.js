e5ec92a8429f8d408009913a00ac62f38c462fa4
Shandira <shandira.ferguson@gmail.com>
2023-02-25 07:48:38 -0800

    prices array added to each

--- renewalroom/src/services.js
+++ renewalroom/src/services.js
@@ -5,8 +5,51 @@ export const ServicesData = [
     image: "./services/massage.jpg",
     alt: 'Massage Image',
     quantity: 0,
-    price: 140,
-    info: "Offering different types of massages such as Swedish, deep tissue, hot stone, aromatherapy, and sports massage."
+    price: 120,
+    info: "Indulge in the ultimate relaxation experience at our spa, where we offer a wide range of massage therapies to suit your individual needs. Our expert massage therapists specialize in a variety of techniques, including the gentle, soothing strokes of Swedish massage, the deep pressure of deep tissue massage, the healing warmth of hot stone massage, the invigorating scents of aromatherapy massage, and the targeted pressure points of sports massage. With our customized approach, you can choose the massage that best fits your goals and preferences, whether it's to unwind and de-stress, alleviate muscle tension, or boost athletic performance. Let us pamper you with our luxurious massages and leave feeling rejuvenated, refreshed, and ready to take on the day.",
+    offerings: 'Swedish, Deep Tissue, Hot Stone, Aromatherapy, and Sports Massage',
+    prices: [
+      {
+        swedish60: '60-minute Swedish massage',
+        price: 80
+      },
+      {
+        swedish90: '90-minute Swedish massage',
+        price: 110
+      },
+      {
+        deep60: '60-minute deep tissue massage',
+        price: 90
+      },
+      {
+        deep90: '90-minute deep tissue massage',
+        price: 120
+      },
+      {
+        stone60: '60-minute hot stone massage',
+        price: 100
+      },
+      {
+        stone90: '90-minute hot stone massage',
+        price: 130
+      },
+      {
+        aroma60: '60-minute aromatherapy massage',
+        price: 85
+      },
+      {
+        aroma90: '90-minute aromatherapy massage',
+        price: 115
+      },
+      {
+        sports60: '60-minute sports massage',
+        price: 90
+      },
+      {
+        sports90: '90-minute sports massage',
+        price: 120
+      },
+    ]
   },
   {
     id: 2,
@@ -15,7 +58,26 @@ export const ServicesData = [
     alt: 'Facials Image',
     quantity: 0,
     price: 50,
-    info: "Offering different types of facials such as basic, deep cleaning, anti-aging, and revitalizing"
+    info: "Unleash your inner radiance with our range of personalized facial treatments. Our expert estheticians specialize in a variety of facials, including the essential cleansing of a basic facial, the transformative benefits of a deep cleaning facial, the rejuvenating effects of an anti-aging facial, and the refreshing glow of a revitalizing facial. Our customized approach allows you to select the facial treatment that best fits your unique skincare needs, from improving skin texture and reducing fine lines to achieving a radiant, youthful complexion. Using the finest ingredients and cutting-edge techniques, we guarantee to leave your skin feeling nourished, refreshed, and positively glowing. Come indulge in our luxurious facial treatments and experience the ultimate pampering session for your skin.",
+    offerings: 'Basic, Deep Cleaning, Anti-aging, and Revitalizing',
+    prices: [
+      {
+        basic: '90-minute Basic facial',
+        price: 100
+      },
+      {
+        deepClean: '90-minute Deep cleaning facial',
+        price: 130
+      },
+      {
+        antiAging: '90-minute Anti-aging facial',
+        price: 200
+      },
+      {
+        revitalizing: '90-minute Revitalizing facial',
+        price: 150
+      }
+    ]
   },
   {
     id: 3,
@@ -24,7 +86,22 @@ export const ServicesData = [
     alt: 'Body Treatments Image',
     quantity: 0,
     price: 80,
-    info: "Offering different types of body treatments such as scrubs, wraps, and hydrotherapy"
+    info: "Experience the ultimate pampering session for your body with our indulgent range of body treatments. Our expert therapists specialize in a variety of treatments, including the exfoliating benefits of body scrubs, the nourishing effects of body wraps, and the revitalizing power of hydrotherapy. Our customized approach allows you to select the treatment that best fits your unique needs, whether it's to rejuvenate your skin, boost your energy, or soothe your senses. Using only the finest ingredients and techniques, we guarantee to leave you feeling refreshed, invigorated, and renewed. Come indulge in our luxurious body treatments and let us pamper you from head to toe.",
+    offerings: 'Scrubs, Wraps, and Hydrotherapy',
+    prices: [
+      {
+        scrub: '90-minute Body Scrub',
+        price: 120
+      },
+      {
+        wrap: '90-minute Body Wrap',
+        price: 150
+      },
+      {
+        hydro: '90-minute Hydrotherapy',
+        price: 200
+      }
+    ]
   },
   {
     id: 4,
@@ -33,7 +110,22 @@ export const ServicesData = [
     alt: 'Mani-Pedi Image',
     quantity: 0,
     price: 55,
-    info: "Offering a variety of nail services such as basic, spa, and gel."
+    info: "Revamp your nails with our luxurious nail services that cater to your unique style and preferences. Whether you're looking for a basic manicure, a relaxing spa pedicure, or a long-lasting gel polish, our expert nail technicians will give you the pampering you deserve. We use only the highest-quality products and techniques to ensure that your nails are healthy, strong, and gorgeous. Whether you want to add a pop of color or a touch of glam to your nails, we've got you covered. Book an appointment today and let us give you the ultimate nail experience that you'll never forget.",
+    offerings: 'Basic, Spa, and Gel packages',
+    prices: [
+      {
+        basicMani: 'Basic manicure',
+        price: 25
+      },
+      {
+        spaMani: 'Spa manicure',
+        price: 50
+      },
+      {
+        gelMani: 'Gel manicure',
+        price: 55
+      }
+    ]
   },
   {
     id: 5,
@@ -42,7 +134,26 @@ export const ServicesData = [
     alt: 'Waxes Image',
     quantity: 0,
     price: 40,
-    info: "Offering different types of waxing services such as leg, arm, bikini, and facial waxing."
+    info: "Get silky-smooth skin with our top-notch waxing services that are tailored to your needs. Our experienced estheticians use gentle techniques and high-quality products to provide you with a comfortable and effective hair removal experience. Whether you're looking for a full leg or arm wax, a Brazilian or bikini wax, or facial waxing services, we've got you covered. Our goal is to help you achieve a flawless and long-lasting result that leaves you feeling confident and beautiful. Book your appointment today and say goodbye to unwanted hair!",
+    offerings: 'Legs, Arms, Bikini, and Facial Waxing',
+    prices: [
+      {
+        legs: '60-minute legs waxing',
+        price: 80
+      },
+      {
+        arms: '60-minute arms waxing',
+        price: 40
+      },
+      {
+        bikini: '90-minute bikini waxing',
+        price: 90
+      },
+      {
+        facial: '60-minute facial waxing',
+        price: 50
+      }
+    ]
   },
   {
     id: 6,
@@ -51,7 +162,26 @@ export const ServicesData = [
     alt: 'Meditation Image',
     quantity: 0,
     price: 120,
-    info: "Offering yoga or meditation classes to help with relaxation and rejuvenation of mind and body."
+    info: "Looking for a way to reduce stress, increase mindfulness, and promote overall well-being? Look no further than our yoga and meditation classes! Our experienced instructors guide you through a series of poses and breathing exercises designed to help you connect with your body and mind. Whether you're a beginner or an experienced yogi, we offer a variety of classes that cater to your needs and goals. With a focus on relaxation and rejuvenation, our classes help you cultivate inner peace, reduce anxiety, and increase flexibility and strength. Come join us for a transformative and energizing experience!",
+    offerings: 'Yoga or Meditation classes',
+    prices: [
+      {
+        medi60: '60-minute meditation',
+        price: 80
+      },
+      {
+        medi90: '90-minute meditation',
+        price: 120
+      },
+      {
+        yoga60: '60-minute yoga',
+        price: 90
+      },
+      {
+        yoga90: '90-minute facial waxing',
+        price: 130
+      }
+    ]
   }
 ]
 
